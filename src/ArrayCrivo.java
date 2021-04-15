e5b3353e21a991fa3d292c2c2ec493e1b5671074
souzags <gugasarraf@gmail.com>
2021-04-15 19:31:13 -0300

    new file:   bin/ArrayCrivo.class 	new file:   src/ArrayCrivo.java

---
+++ src/ArrayCrivo.java
@@ -0,0 +1,45 @@
+/*
+ * Autor: souzags
+ * 
+ * Crivo de Eratóstenes implementado com ArrayList
+ *
+ */
+
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class ArrayCrivo {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		ListIterator iter;
+		int max;
+
+		max = sc.nextInt();
+
+		for(int i = 1; i <= max; i++) list.add(i); // preenche a lista
+
+		list.remove(0); // remove o número 1
+
+		while (list.size() != 0) {
+			iter = list.listIterator(); // reseta o iterator para o início da lista
+			int num = list.get(0);
+			
+			sb.append(num);
+			sb.append(" ");
+
+			while(iter.hasNext()) {
+				if((Integer)iter.next() % num == 0) iter.remove(); // remove todos os múltiplos do primo atual
+			}
+		}
+
+		sb.deleteCharAt(sb.length() - 1); // remove o último espaço em branco
+
+		System.out.println(sb.toString());
+
+	}
+}
