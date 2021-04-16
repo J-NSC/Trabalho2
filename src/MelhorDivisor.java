8ecfa17925ab6510e6e7e0f52e01ba8c39c0cf57
souzags <gugasarraf@gmail.com>
2021-04-16 13:21:30 -0300

    Modificado o algorítmo pra somar os dígitos

--- src/MelhorDivisor.java
+++ src/MelhorDivisor.java
@@ -1,38 +1,36 @@
 import java.util.*;
-
 import java.util.Scanner;
+
 public class MelhorDivisor {
     
-    public int somaDigito(int num) {
-int soma=0;
-for(int i = 0; i <= 3; i++) {
- soma += num % 10;
-num /= 10;
-}
-return soma;
+	public int somaDigito(int num) {
+		int soma=0;
+		while (num > 0) {
+			soma += num % 10;
+			num /= 10;
+		}
+		return soma;
     }
+    
     public static void main(String[] args) {
-Scanner entrada = new Scanner(System.in);
-MelhorDivisor digito = new MelhorDivisor();
+		Scanner entrada = new Scanner(System.in);
+		MelhorDivisor digito = new MelhorDivisor();
+		int num;
+		ArrayList<Integer> number = new ArrayList<Integer>();
+		ArrayList<Integer> somas = new ArrayList<Integer>();
+		num = entrada.nextInt();
 
-int num=0; 
-ArrayList<Integer> number = new ArrayList<Integer>();
-ArrayList<Integer> somas = new ArrayList<Integer>();
- num = entrada.nextInt();
-
-for(int i = 1; i < num; i++) {
-if(num % i == 0) {
-number.add(i);
-somas.add(digito.somaDigito(number.get(number.indexOf(i))));
-}
-}
+		for(int i = 1; i <= num; i++) {
+			if(num % i == 0) {
+				number.add(i);
+				somas.add(digito.somaDigito(i));
+			}
+		}
 
 
-for(int i = 0; i < number.size(); i++) {
-
-System.out.println(somas.get(i));
-
-}
+		for(int i = 0; i < number.size(); i++) {
+			System.out.println(number.get(i) + "  " + somas.get(i));
+		}
 
     }
 }
