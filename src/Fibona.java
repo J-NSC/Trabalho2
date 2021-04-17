fb9c8800a14de71b25ca135a05848eab7b1e856d
sevirotimo <jncosta201@gmail.com>
2021-04-17 00:30:21 -0300

    fibonnace completo

--- src/Fibona.java
+++ src/Fibona.java
@@ -1,20 +1,44 @@
 import java.util.ArrayList;
 
+import javax.naming.spi.DirStateFactory.Result;
+
+
 public class Fibona {
 
 	public static void main(String[] args) {
-		ArrayList<Integer> list = new ArrayList<>();
+		ArrayList<Integer> list = new ArrayList<>(10);
+		long a=9,b=8, result= 0;
+		// for(int i = 0; i < 9; i++){
+		// 	list.add(Fibo(i, i+1));
+		// }
+
+
+		// list.add(1);
+		// list.add(11);
+	
+		// a = list.indexOf(13);
+		// b = list.get(a);
+		// System.out.println("rank do a" +a);
+		// System.out.println("valor contido no"+ b);
+
 		
-		for(int i = 0; i < 9; i++){
-			list.add(Cal.Fibo(i));
-		}
-
-		System.out.println(list.get(1));
-
-
+		System.out.println(fibo(3, 7, 5));
+		
 	}
 
-	
+
+	public static int fibo (int a, int b, int n){
+		int res =0;
+		for(int i=0; i < n-1; i++){
+			res = a +b;
+			a=b;
+			b=res;
+		}
+
+		return res;
+	}
+
+
 
 }
 
