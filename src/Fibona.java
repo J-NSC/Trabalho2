e7b780f38c0d9146aa971399a3fc5ec1b895e2b3
J <58124203+sevirotimo@users.noreply.github.com>
2021-04-17 16:47:49 -0300

    Update Fibona.java

--- src/Fibona.java
+++ src/Fibona.java
@@ -6,22 +6,6 @@ import javax.naming.spi.DirStateFactory.Result;
 public class Fibona {
 
 	public static void main(String[] args) {
-		ArrayList<Integer> list = new ArrayList<>(10);
-		long a=9,b=8, result= 0;
-		// for(int i = 0; i < 9; i++){
-		// 	list.add(Fibo(i, i+1));
-		// }
-
-
-		// list.add(1);
-		// list.add(11);
-	
-		// a = list.indexOf(13);
-		// b = list.get(a);
-		// System.out.println("rank do a" +a);
-		// System.out.println("valor contido no"+ b);
-
-		
 		System.out.println(fibo(3, 7, 5));
 		
 	}
