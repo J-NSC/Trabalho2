2068b82b83b6f2d9275eb084873cf411bb91abf4
souzags <gugasarraf@gmail.com>
2021-04-17 15:03:11 -0300

    Corrigida a falha no algorítmo

--- src/Jarros.java
+++ src/Jarros.java
@@ -33,15 +33,25 @@ public class Jarros {
 	// Este método diz se é possivel encher A ou B com C litros
 	public static String giveAns(int a, int b, int c) {
 
-		if (c == a || c == b) return "SIM"; // Se qualquer um dos recipientes tiver o volume desejado, é possível
+		// Se qualquer um dos recipientes tiver o volume desejado, é possível
+		if (c == a || c == b) return "SIM";
 
-		else if (c > a && c > b) return "NÃO"; // Se os dois recipientes forem menores que o volume desejado, não é possivel
+		// Se os dois recipientes forem menores que o volume desejado, não é possivel
+		else if (c > a && c > b) return "NÃO";
 
-		else if (a == b) return "NÃO"; // Se os recipientes forem iguais, não é possível
+		// Se os recipientes forem iguais, não é possível
+		else if (a == b) return "NÃO";
 
-		else if (a < b && b % a == 0) return "NÃO"; // Se o maior recipiente for múltiplo do menor...
+		// Se o maior recipiente for múltiplo do menor...
+		else if (a < b && b % a == 0) {
+			if (c % a == 0) return "SIM"; // ...e se o volume desejado for múltiplo do menor, é possível...
+			else return "NÃO"; // ...senão, não é possível
+		}
 
-		else if (a > b && a % b == 0) return "NÃO"; // Não é possível
+		else if (a > b && a % b == 0) {
+			if (c % a == 0) return "SIM";
+			else return "NÃO";
+		}
 
 		else return "SIM"; // Para qualquer outro caso, é possivel
 
