b2a36b9118ce98431a88d72bedb9b3c151bbd476
sevirotimo <jncosta201@gmail.com>
2021-04-22 16:09:14 -0300

    Subi o projeto, errado esse´e o certo

--- src/FatoresPrimos.java
+++ src/FatoresPrimos.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class FatoresPrimos {
 	public static void main(String[] args) {
-		System.out.println("Hello, git!");
+
+		Scanner scan = new Scanner (System.in);
+		int c=0, n=0;
+
+		c = scan.nextInt();
+
+		for(int i = 0; i < 2; i++ ){
+			if(c % 2 != 0){
+				n++;
+			}
+		}
+
+		System.out.println(n);
 	}
 }
